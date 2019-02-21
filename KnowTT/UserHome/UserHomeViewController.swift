1e3236ebaeb0a58d8f1989a7f96872628f939882
Cris <cris.kgl@gmail.com>
2019-02-21 01:52:41 -0800

    Map added to track location to send notes, added interface to send notes, started the wrapping to convert message to JSON. Also added log field to track behaviour of network

--- KnowTT/UserHome/UserHomeViewController.swift
+++ KnowTT/UserHome/UserHomeViewController.swift
@@ -12,11 +12,16 @@ import FirebaseAuth
 import FirebaseDatabase
 import SocketIO
 import SwiftSocket
+import MapKit
 
-class UserHomeViewController: UIViewController{
+class UserHomeViewController: UIViewController, CLLocationManagerDelegate{
 
     @IBOutlet weak var tcpLog: UITextField!
     @IBOutlet weak var userLoggedInText: UILabel!
+    @IBOutlet weak var userMap: MKMapView!
+    
+    //LOCATION MANAGER
+    let coordinatesManager = CLLocationManager()
     
     var userMail = Auth.auth().currentUser!.email
     var userDefault = "user unknown"
@@ -24,7 +29,15 @@ class UserHomeViewController: UIViewController{
     var client: TCPClient?
     
     override func viewDidLoad() {
-        userLoggedInText.text = "User: \(userMail ?? userDefault)"
+        //Ask user to start tracking his position
+        self.coordinatesManager.requestAlwaysAuthorization()
+        //if the user allows, set the delegate to the same coordinatesManager
+        if CLLocationManager.locationServicesEnabled(){
+            coordinatesManager.delegate = self
+            coordinatesManager.desiredAccuracy = kCLLocationAccuracyBest
+            coordinatesManager.startUpdatingLocation()
+        }
+        userLoggedInText.text = "Welcome \(userMail ?? userDefault) !"
 
         //Invoking TCP client
         client = TCPClient(address: "142.93.204.52", port: 8080)
@@ -53,6 +66,34 @@ class UserHomeViewController: UIViewController{
         case .failure(let error):
             appendToTextField(string: String(describing: error))
         }
+    }
+    
+    @IBAction func addNoteTouched(_ sender: Any) {
+        //Get locations from user
+        let coordinate =  CLLocationCoordinate2D(latitude: UserDefaults.standard.value(forKey: "LAT") as! CLLocationDegrees, longitude: UserDefaults.standard.value(forKey: "LON") as! CLLocationDegrees)
+        print("GOOOOTEEEEM:\n Lat: \(coordinate.latitude) \n Long: \(coordinate.longitude)")
+        //1. Create the alert controller.
+        
+        let alert = UIAlertController(title: "New KnowT", message: "", preferredStyle: .alert)
+        //2. Add the text field. You can configure it however you need.
+        alert.addTextField { (textField) in
+            textField.text = "Your note goes here"
+        }
+        alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler:{(action) in alert.dismiss(animated: true, completion: nil)}))
+        // 3. Grab the value from the text field, and SEND IT TO SERVER
+        alert.addAction(UIAlertAction(title: "KnowtIt", style: .default, handler:
+            {
+                [weak alert] (_) in
+                let textField = alert?.textFields![0] // Force unwrapping because we know it exists.
+                #warning ("Implemennt wrapping of data")
+                #warning ("Send to server with opcode 1")
+                
+                print("Text field: \(String(describing: textField?.text))")
+            }))
+        
+        // 4. Present the alert.
+        self.present(alert, animated: true, completion: nil)
+ 
     }
     
     private func sendRequest(string: String, using client: TCPClient) -> String? {
@@ -88,8 +129,21 @@ class UserHomeViewController: UIViewController{
         print(string)
         tcpLog.text = tcpLog.text?.appending("\n\(string)")
     }
-
     
+    //Track the user position in real time and display it on the map
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //let userPosition:CLLocationCoordinate2D = manager.location!.coordinate
+        //print("locations = \(userPosition.latitude) \(userPosition.longitude)")
+        let userLocation = locations.last
+        let viewRegion = MKCoordinateRegion(center: (userLocation?.coordinate)!, latitudinalMeters: 100, longitudinalMeters: 100)
+        self.userMap.setRegion(viewRegion, animated: true)
+        self.userMap.showsUserLocation = true
+        
+        //Save current lat & long
+        UserDefaults.standard.set(userLocation?.coordinate.latitude, forKey: "LAT")
+        UserDefaults.standard.set(userLocation?.coordinate.longitude, forKey: "LON")
+        UserDefaults().synchronize()
+    }
     
 }
 
