7c7de2f0e01c8717b6f62250a3bfb00a1a61e675
Cris <cris.kgl@gmail.com>
2019-02-20 20:52:10 -0800

    Client can now send Strings to server

--- KnowTT/UserHome/UserHomeViewController.swift
+++ KnowTT/UserHome/UserHomeViewController.swift
@@ -14,21 +14,20 @@ import SocketIO
 import SwiftSocket
 
 class UserHomeViewController: UIViewController{
+
+    @IBOutlet weak var tcpLog: UITextField!
+    @IBOutlet weak var userLoggedInText: UILabel!
     
     var userMail = Auth.auth().currentUser!.email
     var userDefault = "user unknown"
+    //Essence of client
+    var client: TCPClient?
     
-    //Set up socket manager
-    
-    //let manager = SocketManager(socketURL: URL(string: "http://localhost:5056")!, config: [.log(true), .compress])
-    //var socket: SocketIOClient!
-    
-    @IBOutlet weak var userLoggedInText: UILabel!
     override func viewDidLoad() {
-        //self.socket = manager.defaultSocket;
         userLoggedInText.text = "User: \(userMail ?? userDefault)"
-        //socket.connect()
 
+        //Invoking TCP client
+        client = TCPClient(address: "142.93.204.52", port: 8080)
         super.viewDidLoad()
     }
     @IBAction func logOutTapped(_ sender: Any) {
@@ -43,32 +42,37 @@ class UserHomeViewController: UIViewController{
         }
     
     @IBAction func connectTapped(_ sender: Any) {
-        
-
-        
-        //TCPswiftSocket------Down
-        let client = TCPClient(address: "142.93.204.52", port: 8080)
-        switch client.connect(timeout: 1) {
+        //Trying to send messages
+        guard let client = client else{return}
+        switch client.connect(timeout: 10) {
         case .success:
-            print("IOs App succesfully connected to Server")
-            switch client.send(string: "Goooooooteeeeeemmmmm" ) {
-            case .success:
-                guard let data = client.read(1024*10) else
-                {
-                    return
-                }
-                
-                if let response = String(bytes: data, encoding: .utf8) {
-                    print(response)
-                }
-            case .failure(let error):
-                print(error)
+            appendToTextField(string: "Connected to host")
+            if let response = sendRequest(string:"Hey there, Im the Iphone!", using: client){
+                appendToTextField(string: "Response: \(response)")
             }
         case .failure(let error):
-            print(error)
+            appendToTextField(string: String(describing: error))
         }
+    }
+    
+    private func sendRequest(string: String, using client: TCPClient) -> String? {
+        appendToTextField(string: "Iphone Sending Data . . .")
         
-        //TCPswiftSocket------Finished
+        switch client.send(string: string) {
+        case .success:
+            appendToTextField(string: "The data has been succesfully sent")
+            return readResponse(from: client) //This is returning the string read from server
+        case .failure(let error):
+            print("error sending data from Iphone")
+            appendToTextField(string: String(describing: error))
+            return nil
+        }
+    }
+    
+    private func readResponse(from client: TCPClient) -> String? {
+        guard let response = client.read(1024*10) else {return nil}
+        print("SERVER RESPONSE: \(response)")
+        return String(bytes: response, encoding: .utf8)
     }
     
     
@@ -79,10 +83,39 @@ class UserHomeViewController: UIViewController{
         
         self.present(alert, animated: true, completion: nil)
     }
+    
+    private func appendToTextField(string: String){
+        print(string)
+        tcpLog.text = tcpLog.text?.appending("\n\(string)")
+    }
 
     
     
 }
-    
-    
 
+/*
+//TCPswiftSocket------Down
+let client = TCPClient(address: "142.93.204.52", port: 8080)
+switch client.connect(timeout: 1) {
+case .success:
+    print("IOs App succesfully connected to Server")
+    switch client.send(string: "Goooooooteeeeeemmmmm" ) {
+    case .success:
+        guard let data = client.read(1024*10) else
+        {
+            return
+        }
+        
+        if let response = String(bytes: data, encoding: .utf8) {
+            print(response)
+        }
+    case .failure(let error):
+        print(error)
+    }
+case .failure(let error):
+    print(error)
+}
+
+//TCPswiftSocket------Finished
+    
+*/
