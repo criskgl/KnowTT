789a7497d95c1072978f2a5f2320299f82cae5e6
Cris <cris.kgl@gmail.com>
2019-03-05 02:04:43 -0800

    Polished error messages in forgot password

--- KnowTT/Authentication/ForgorPassword.swift
+++ KnowTT/Authentication/ForgorPassword.swift
@@ -21,18 +21,32 @@ class ForgotPassword: UIViewController {
         self.hideKeyboardWhenTappedAround() 
     }
     @IBAction func sendResetEmailTouched(_ sender: Any) {
-        guard
+        guard //check for blank input
             emailUser.text!.count > 0
             else{
                 SCLAlertView().showWarning("Please provide your email", subTitle: "")
                 return
+            }
+        guard
+            isValidEmail(testStr: emailUser.text!)
+         == true
+        else{
+            SCLAlertView().showWarning("Not a ucsb.edu email", subTitle: "")
+            return
         }
         Auth.auth().sendPasswordReset(withEmail: emailUser.text!) { error in
             if error != nil {//ERROR
-                SCLAlertView().showError("Error sending password reset email", subTitle: "")
+                SCLAlertView().showError("Error sending password reset email", subTitle: "The email provided might not be in our database")
             }else{
                 SCLAlertView().showSuccess("Email to reset password sent", subTitle: "")
             }
         }
+    }
+    
+    private func isValidEmail(testStr:String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@ucsb+\\.edu"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: testStr)
     }
 }
